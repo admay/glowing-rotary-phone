441623608dbf30159322215b0b854b7a55d54206
Michael <zavarella.dev@gmail.com>
2017-01-27 01:38:51 +0000

    Fully working todo-list REST web service

--- src/pedestal_todo/core.clj
+++ src/pedestal_todo/core.clj
@@ -3,39 +3,142 @@
             [io.pedestal.http.route :as route]
             [io.pedestal.test :as test]))
 
-(defn response [status body & {:as headers}]
-  {:status status :body (with-out-str (pprint body)) :headers headers})
+(defn response
+  "Create a response"
+  [status body & {:as headers}]
+  {:status status :body body :headers headers})
 
 (def ok       (partial response 200))
 (def created  (partial response 201))
 (def accepted (partial response 202))
 
-(def echo
-  {:name :echo
+;;;
+;;; "Database" functions
+;;;
+(defonce database (atom {}))
+
+(defn find-list-by-id
+  "Query DB for a list"
+  [dbval db-id]
+  (get dbval db-id))
+
+(defn find-list-item-by-ids
+  "Query DB for list item"
+  [dbval list-id item-id]
+  (get-in dbval [list-id :items item-id] nil))
+
+(defn list-item-add
+  "Add a list item to DB"
+  [dbval list-id item-id new-item]
+  (if (contains? dbval list-id)
+    (assoc-in dbval [list-id :items item-id] new-item)
+    dbval))
+
+(def db-interceptor
+  {:name :database-interceptor
    :enter
    (fn [context]
-     (let [request (context :request) ;; why even bother?
-           response (ok context)]
-       (assoc context :response response)))})
+     (update context :request assoc :database @database))
+   :leave
+   (fn [context]
+     (if-let [[op & args] (:tx-data context)]
+       (do
+         (apply swap! database op args)
+         (assoc-in context [:request :database] @database))
+       context))})
+
+;;;
+;;; Domain functions
+;;;
+(defn make-list
+  "Create list map"
+  [name]
+  {:name  name
+   :items {}})
+
+(defn make-list-item
+  "Create list item map"
+  [name]
+  {:name  name
+   :done? false})
+
+;;;
+;;; API Interceptors
+;;;
+(def entity-render
+  {:name :entity-render
+   :leave
+   (fn [context]
+     (if-let [item (:result context)]
+       (assoc context :response (ok item))
+       context))})
+
+(def list-create
+  {:name :list-create
+   :enter
+   (fn [context]
+     (let [name     (get-in context [:request :query-params :name] "Unnamed List")
+           new-list (make-list name)
+           db-id    (str (gensym "l"))
+           url      (route/url-for :list-view :params {:list-id db-id})]
+       (assoc context
+              :response (created new-list "Location" url)
+              :tx-data [assoc db-id new-list])))})
+
+(def list-view
+  {:name :list-view
+   :enter
+   (fn [context]
+     (if-let [db-id (get-in context [:request :params :list-id])]
+       (if-let [l (find-list-by-id (get-in context [:request :database]) db-id)]
+         (assoc context :result l)
+         context)
+       context))})
+
+(def list-item-view
+  {:name :list-item-view
+   :leave
+   (fn [context]
+     (if-let [list-id (get-in context [:request :path-params :list-id])]
+       (if-let [item-id (get-in context [:request :path-params :item-id])]
+         (if-let [item (find-list-item-by-ids (get-in context [:request :database]) list-id item-id)]
+           (assoc context :result item)
+           context)
+         context)
+       context))})
+
+(def list-item-create
+  {:name :list-item-create
+   :enter
+   (fn [context]
+     (if-let [list-id (get-in context [:request :path-params :list-id])]
+       (let [nm       (get-in context [:request :query-params :name] "Unnamed Item")
+             new-item (make-list-item nm)
+             item-id  (str (gensym "i"))]
+         (-> context
+             (assoc :tx-data  [list-item-add list-id item-id new-item])
+             (assoc-in [:request :path-params :item-id] item-id)))
+       context))})
 
 (def routes
   (route/expand-routes
-   #{["/todo"                 :post   echo :route-name :list-create]
+   #{["/todo"                 :post   [db-interceptor list-create]]
      ["/todo"                 :get    echo :route-name :list-query-form]
-     ["/todo/:list-id"        :get    echo :route-name :list-view]
-     ["/todo/:list-id"        :post   echo :route-name :list-item-create]
-     ["/todo/:list-id/:item"  :get    echo :route-name :list-item-view]
+     ["/todo/:list-id"        :get    [entity-render db-interceptor list-view]]
+     ["/todo/:list-id"        :post   [entity-render list-item-view db-interceptor list-item-create]]
+     ["/todo/:list-id/:item"  :get    [entity-render list-item-view]]
      ["/todo/:list-id/:item"  :put    echo :route-name :list-item-update]
      ["/todo/:list-id/:item"  :delete echo :route-name :list-item-delete]}))
 
 (def service-map
   {::http/routes routes
-   ::http/type :jetty
-   ::http/port 8080})
+   ::http/type   :jetty
+   ::http/port   8890})
 
 (defn start []
   (http/start (http/create-server service-map)))
 
+;; For interactive development
 (defonce server (atom nil))
 
 (defn start-dev []
@@ -50,3 +153,6 @@
 (defn restart []
   (stop-dev)
   (start-dev))
+
+(defn test-request [verb url]
+  (io.pedestal.test/response-for (::http/service-fn @server) verb url))
